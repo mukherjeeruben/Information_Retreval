fd5eae42e8d796c4ebfbf0f4c6ae7ff0588988bd
mukherjeeruben <rubenmukherjee@outlook.com>
2022-02-24 23:54:11 +0000

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-document_directory = '''D:\\Dublin_City_University\\CA6005\\new_data\\COLLECTION''' ## Set Local Directory
-query_directory = '''D:\\Dublin_City_University\\CA6005\\new_data\\topics''' ## Set Local Directory
+document_directory = '''D:\\Dublin_City_University\\CA6005\\data\\COLLECTION''' ## Set Local Directory
+query_directory = '''D:\\Dublin_City_University\\CA6005\\data\\topics''' ## Set Local Directory
 
